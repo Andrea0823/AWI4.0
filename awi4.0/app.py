0bc5bd89cc44abcefac778f039c38d52c00434b2
Andrea Martínez García <97360188+Andrea0823@users.noreply.github.com>
2023-01-19 14:50:43 +0000

    estructura básica para un formulario

--- awi4.0/app.py
+++ awi4.0/app.py
@@ -15,6 +15,24 @@ class Bienvenida:
     def GET(self):
         return render.bienvenida()
 
+#Formulario
+import web
+
+urls = (
+    '/', 'Index',
+
+)
+app = web.application(urls, globals())
+render = web.template.render('templetes')
+class Index:
+    def GET(self):
+        return render.formulario()
+
+    def POST(self):
+        form = web.input()
+        print(type(form))
+        return form["nombre"]
+
 
 if __name__ == "__main__":
     app.run()
