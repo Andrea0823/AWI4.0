ff57abc80ba8869bcd9c4e5f7d7b2149bda6dad0
Andrea Martínez García <97360188+Andrea0823@users.noreply.github.com>
2023-01-26 04:35:34 +0000

    Tablas

--- awi4.0/app.py
+++ awi4.0/app.py
@@ -96,10 +96,10 @@ render = web.template.render('templates')
 class Tablas:
     def GET(self):
         datos = [
-            ["1","Dejah"]
-            ["2","Jhon"]
+            ["1","Dejah"],
+            ["2","Jhon"],
         ]
-        return render.tablas(datos)
+        return render.Tablas(datos)
 
 if __name__ == "__main__":
     # web.config.debug=False
